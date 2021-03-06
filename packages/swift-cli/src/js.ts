282579e20673546b7e59fc36093ba304533e766b
Haishan <haishanhan@gmail.com>
2021-03-06 18:23:00 +0800

    Provide input source map to terser

--- packages/swift-cli/src/js.ts
+++ packages/swift-cli/src/js.ts
@@ -1,5 +1,6 @@
 import { relative } from "path";
 import { promises } from "fs";
+import { MinifyOutput } from "terser";
 
 import { writeFileSafely } from "./utils";
 import * as config from "./config";
@@ -20,27 +21,40 @@ export async function processJS(inputPath: string, outputPath: string) {
     filename: inputPath,
     sourceMaps: true,
   });
+
   if (!ret || !ret.code) {
-    // many times it's became this is a new file
+    // many times it's because this is a new file
     return;
   }
 
+  // to debug source map
+  // use https://evanw.github.io/source-map-visualization/
+
   let code = ret.code;
+  let map: MinifyOutput["map"];
+
+  if (ret.map) {
+    map = ret.map;
+  }
 
   if (!config.get("isDev")) {
-    const minifyRet = await minifier.transform(ret.code);
+    const minifierOptions = ret.map ? { sourceMap: { content: ret.map } } : {};
+    const minifyRet = await minifier.transform(ret.code, minifierOptions);
     if (!minifyRet.code) {
       throw new Error(`${inputPath} terser transform result is empty`);
     }
     code = minifyRet.code;
+    if (minifyRet.map) {
+      map = minifyRet.map;
+    }
   }
 
   const tasks = [writeFileSafely(outputPath, code)];
 
-  let map = ret.map;
   if (map) {
-    map.sourceRoot = "";
-    tasks.push(writeFileSafely(outputPath + ".map", JSON.stringify(map)));
+    // map.sourceRoot = "";
+    const cnt = typeof map === "string" ? map : JSON.stringify(map);
+    tasks.push(writeFileSafely(outputPath + ".map", cnt));
   }
 
   await Promise.all(tasks);
--- packages/swift-cli/src/minify.ts
+++ packages/swift-cli/src/minify.ts
@@ -1,14 +1,15 @@
-import { minify } from "terser";
+import { minify, MinifyOptions } from "terser";
 
-export async function transform(code: string) {
+export async function transform(code: string, opt?: Partial<MinifyOptions>) {
   const options = {
-    // mangle: { toplevel: true },
+    toplevel: true,
     mangle: true,
     safari10: true,
     compress: { passes: 2 },
     output: {
       comments: false,
     },
+    ...opt,
   };
   return await minify(code, options);
 }
